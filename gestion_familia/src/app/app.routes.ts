2e25580ec28fb2cae9081edc67ce108227417395
Gutierrez-Ignacio <99gutierrezignacio@gmail.com>
2024-07-20 16:30:56 -0300

    Add login and operations components

--- gestion_familia/src/app/app.routes.ts
+++ gestion_familia/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { OperationsComponent } from './pages/operations/operations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },   
+    { path: 'vOperaciones', component: OperationsComponent}
+];
